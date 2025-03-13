f4c167c04e5defb3010196a1574a78abbd71f051
Jhonny Miranda <jhonnyrs22@hotmail.com>
2025-03-13 13:38:08 -0600

    solución de errores de routing react

--- src/Components/Nav_Foot/Navbar.jsx
+++ src/Components/Nav_Foot/Navbar.jsx
@@ -1,18 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+// Cambia la exportación
+export const Navbar = () => {
   return (
-    <nav className="bg-[#D5C4A1] p-4 rounded-b-xl flex justify-center">
-      <ul className="flex space-x-6 text-[#4D3B2A] font-semibold">
-        <li><Link to="/" className="hover:underline">INICIO</Link></li>
-        <li><Link to="/menu" className="hover:underline">MENÚ</Link></li>
-        <li><Link to="/about" className="hover:underline">ACERCA DE NOSOTROS</Link></li>
-        <li><Link to="/productos" className="hover:underline">PRODUCTOS</Link></li>
-        <li><Link to="/servicios" className="hover:underline">SERVICIOS</Link></li>
-        <li><Link to="/contacto" className="hover:underline">CONTÁCTENOS</Link></li>
-      </ul>
+    <nav className="bg-espresso-brown text-warm-beige py-4">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <Link to="/" className="font-righteous text-2xl">
+          Palenque Garabito
+        </Link>
+        
+        <div className="flex gap-6 font-poppins">
+          <Link to="/" className="hover:text-deep-moss transition">
+            Inicio
+          </Link>
+          <Link to="/menu" className="hover:text-deep-moss transition">
+            Menú
+          </Link>
+          <Link to="/about" className="hover:text-deep-moss transition">
+            Nosotros
+          </Link>
+          <Link to="/contact" className="hover:text-deep-moss transition">
+            Contacto
+          </Link>
+          <Link to="/login" className="hover:text-deep-moss transition">
+            Iniciar Sesión
+          </Link>
+        </div>
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
