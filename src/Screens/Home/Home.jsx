58da9759ed6c3639fcde205abee33c126643c502
LuisCarlosSolis <168393621+LuisCarlosSolis@users.noreply.github.com>
2025-04-04 19:07:35 -0600

    fix: routes

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,6 +1,5 @@
-import { Navbar } from '../Nav_Foot/Navbar';
-import Footer from '../Nav_Foot/Footer'
-import { MainButton, SecondaryButton } from '../ui/Buttons'
+import { Navbar } from '../../Components/Navbar';
+import Footer from '../../Components/Footer.jsx';
 import { Home_Title } from './Home_Components/Home_Title';
 import { Home_Discover } from './Home_Components/Home_Discover';
 
