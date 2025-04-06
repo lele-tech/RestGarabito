d31eb0995a91dd087716c6aa20638bdc3240de7c
Brittany Fallas <103863271+brsflls@users.noreply.github.com>
2025-04-05 19:24:23 -0600

    Create Card_plato
    + Implementación de card para platos

---
+++ src/Components/Card_plato.jsx
@@ -0,0 +1,31 @@
+export const CardPlato = () => {
+    return (
+        <div className="w-96 max-h-fit rounded-2xl shadow-xl bg-[#f3ece1] pt-0 px-0 pb-0 m-5 drop-shadow-lg">
+
+            <div className=" w-96 h-72 bg-[#C5B9A6] rounded-xl drop-shadow-lg">
+                {/* Reemplazar con imagen de plato.
+                <img src={...} className="object-cover w-full h-full rounded-xl" /> */}
+            </div>
+
+            <div className="p-3">
+                <h3 className="font-[Poppins] text-md font-semibold drop-shadow-lg text-[#362419] uppercase">
+                    Nombre del plato 
+                </h3>
+
+                <div className="font-[Poppins] flex items-center text-xs text-[#362419A6] opacity-60 my-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="0.8rem" height="0.8rem" viewBox="0 0 24 24"><g fill="none" stroke="#5D5C26" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"><circle cx="12" cy="12" r="9" /><path d="M11 8v5h5" /></g></svg>
+                    <span className="ml-1">15 Mins</span>
+                </div>
+
+                <div className="flex items-center mt-1 px-1">
+                    <span className="font-[Poppins] text-lg font-bold drop-shadow-lg text-[#362419]">₡0’000</span>
+                    <button className="ml-auto font-[Poppins] drop-shadow-lg text-sm font-bold text-deep-moss hover:underline transition  transition duration-300 hover:scale-105  mr-6">
+                        MÁS INFO
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CardPlato;
