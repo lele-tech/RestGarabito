d63c13e3a7543229c8bb693e40e5ac95e2219f46
Jonavcc <jonathancc21@gmail.com>
2025-04-08 20:57:07 -0600

    add/ se el llamado del componente footer

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,7 +1,12 @@
+<<<<<<< Updated upstream
 import { Navbar } from '../../Components/Navbar';
 import Footer from '../../Components/Footer.jsx';
 import { Home_Title } from './Home_Components/Home_Title';
 import { Home_Discover } from './Home_Components/Home_Discover';
+=======
+import Footer2 from "../../Components/Footer2";
+import Navbar from "../../Components/Navbar";
+>>>>>>> Stashed changes
 
 export function Home() {
     return (
@@ -9,7 +14,10 @@ export function Home() {
       <Home_Title />
       <Home_Discover />
 
-      <Footer />
+      <Footer2 />
+      {/* <div className="footer-bottom-home">
+        <p>© 2025 Palenque Garabito. Todos los derechos reservados</p>
+      </div> */}
     </div>
   );
 }
