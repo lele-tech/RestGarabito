853812ae822ec57f964fef8845ccff2e768c8c30
LuisCarlosSolis <168393621+LuisCarlosSolis@users.noreply.github.com>
2025-04-16 17:35:11 -0600

    add: Pantalla de Agregar Plato

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Home } from "./Screens/Home/Home";
 import { Contact } from "./Screens/Contact/Contact";
 import About from "./Screens/About/About";
 import Navbar from "./Components/Navbar";
-import FormularioPlato from "./Components/FormularioPlato/FormularioPlato";
+import { Agregar_Plato } from "./Screens/AgregarPlato/Agregar_Plato";
 
 function App() {
   return (
@@ -15,16 +15,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-
-          {/* FORMULARIO PLATO, BOTON EDITAR */}
-          {/* <Route path="/menu" element={<FormularioPlato modo="editar" />} /> */}
-
-          {/* FORMULARIO PLATO, BOTON AGREGAR */}
-          <Route path="/menu" element={<FormularioPlato modo="crear" />} />
+          <Route path="/agregar" element={<Agregar_Plato/>} />
           {/* <Route
           path="/menu"
           element={<div>Página de Menú en construcción</div>}
         /> */}
+
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
