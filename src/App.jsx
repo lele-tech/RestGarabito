0bf50deabe955971748807a4bdcf1d86cfe4db1e
Jhonny Miranda <jhonnyrs22@hotmail.com>
2025-04-24 17:50:01 -0600

    update error merge app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,22 @@ import { Home } from "./Screens/Home/Home";
 import { Contact } from "./Screens/Contact/Contact";
 import About from "./Screens/About/About";
 import Navbar from "./Components/Navbar";
-<<<<<<< HEAD
-import { Products } from "./Screens/Products/Products";
-=======
 import { Agregar_Plato } from "./Screens/AgregarPlato/Agregar_Plato";
->>>>>>> main
+import { Products } from "./Screens/Products/Products";
 
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-    <Navbar />
-    <main className="min-h-screen overflow-x-hidden">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/menu" element={<Products />}/>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />}
-        />
-      </Routes>
-=======
       <Navbar />
       <main className="min-h-screen overflow-x-hidden">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/agregar" element={<Agregar_Plato/>} />
-          {/* <Route
-          path="/menu"
-          element={<div>Página de Menú en construcción</div>}
-        /> */}
-
+          <Route path="/menu"element={<Products/>} /> 
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
->>>>>>> main
       </main>
     </BrowserRouter>
   );
