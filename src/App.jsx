457e01dcad6285a052d34c2f37ca5acf80b514b5
Jhonny Miranda <jhonnyrs22@hotmail.com>
2025-04-24 17:13:35 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import { Home } from "./Screens/Home/Home";
 import { Contact } from "./Screens/Contact/Contact";
 import About from "./Screens/About/About";
 import Navbar from "./Components/Navbar";
+<<<<<<< HEAD
 import { Products } from "./Screens/Products/Products";
+=======
+import { Agregar_Plato } from "./Screens/AgregarPlato/Agregar_Plato";
+>>>>>>> main
 
 function App() {
   return (
     <BrowserRouter>
+<<<<<<< HEAD
     <Navbar />
     <main className="min-h-screen overflow-x-hidden">
       <Routes>
@@ -20,6 +25,22 @@ function App() {
         <Route path="/contact" element={<Contact />}
         />
       </Routes>
+=======
+      <Navbar />
+      <main className="min-h-screen overflow-x-hidden">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/agregar" element={<Agregar_Plato/>} />
+          {/* <Route
+          path="/menu"
+          element={<div>Página de Menú en construcción</div>}
+        /> */}
+
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+>>>>>>> main
       </main>
     </BrowserRouter>
   );
